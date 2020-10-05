c934a4e04f7a6da9ca85a83a8f8f81db72e9638a
lchua2314 <lchua2314@gmail.com>
2020-10-04 23:36:17 -0700

    Updated README.md and initalized FinalProgram.java format and imports

---
+++ FinalProgram.java
@@ -0,0 +1,33 @@
+/***************************************************************
+* file: FinalProgram.java
+* author: L. Chua, A. Sun, M. Yang
+* class: CS 4450 – Computer Graphics
+*
+* assignment: final program
+* date last modified: 10/4/20
+*
+* purpose: This program creates a blockly game.
+*
+****************************************************************/
+package finalprogram;
+
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import static org.lwjgl.opengl.GL11.*;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import org.lwjgl.input.Keyboard;
+
+public class FinalProgram {
+
+    // method: main
+    // purpose: Creates an instance of the FinalProgram class. 
+    // This is so we don't have to make all the classes static.
+    // Then the start method is called.
+    public static void main(String[] args) {
+        
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-"# CS-4450-Final-Project" 
+# CS 4450 Final Project
+
+### Requirements
+
+- NetBeans
+- Lightweight Java Game Library (LWJGL) Version 2.9.2
+- Java Development Kit 8 or higher version
+
+### Installation
+
+Download to LWJGL 2.9.2:
+https://github.com/lchua2314/LWJGL-2.9.2
+
+How to install LWJGL for version 2:
+http://wiki.lwjgl.org/wiki/Setting_Up_LWJGL_with_NetBeans.html
+
+### Usage
+
+Run in NetBeans
