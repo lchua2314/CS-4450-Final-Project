e0cdc87e5eba505ca46b8da255e91abc7f055057
lchua2314 <lchua2314@gmail.com>
2020-10-06 00:25:31 -0700

    Copied code from (10) 3D Viewing

--- FinalProgram.java
+++ FinalProgram.java
@@ -11,51 +11,51 @@
 ****************************************************************/
 package finalprogram;
 
+import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import static org.lwjgl.opengl.GL11.*;
-import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import org.lwjgl.input.Keyboard;
+import org.lwjgl.util.glu.GLU;
 
 public class FinalProgram {
     
-    private float dx;
-    private float dy;
-    private float roll;
+    private FPCameraController fp = new FPCameraController(0f,0f,0f);
+    private DisplayMode displayMode;
     
-    public FinalProgram() {
-        dx = 0;
-        dy = 0;
-        roll = 0;
-    }
-
     // method: start
     // purpose: Tries to create the window, initialize GL, and render the image.
-    // If an error is found, the stack will catch and return it, terminating the program.
+    // If an error is found, the stack will catch and return it, terminating the program.    
     public void start() {
-        try {
-            createWindow();
-            initGL();
-            render();
-        } 
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
     
-    // method: createWindow
-    // purpose: Sets fullscreen to false, sets window to be 640x480 pixels 
-    // and sets the title of the window to "Final Program"
-    private void createWindow() throws Exception{
-        Display.setFullscreen(false);
-        Display.setDisplayMode(new DisplayMode(640, 480));
-        Display.setTitle("Final Program");
-        Display.create();
+        try {
+                createWindow();
+                initGL();
+                fp.gameLoop();//render();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
     }
 
+    // method: createWindow
+    // purpose: Sets fullscreen to false, sets window to be 640x480 pixels 
+    // and sets the title of the window to "Final Program"    
+    private void createWindow() throws Exception{
+        Display.setFullscreen(false);
+        DisplayMode d[] =
+        Display.getAvailableDisplayModes();
+        for (int i = 0; i < d.length; i++) {
+            if (d[i].getWidth() == 640
+            && d[i].getHeight() == 480
+            && d[i].getBitsPerPixel() == 32) {
+            displayMode = d[i];
+            break;
+            }
+        }
+        Display.setDisplayMode(displayMode);
+        Display.setTitle("Hey Mom! I am using” + “OpenGL!!!");
+        Display.create();
+    }
+    
     // method: initGL
     // purpose: Selects the background color, Alpha, 
     // loads camera and Identity matrix, sets up orthographic matrix,
@@ -64,107 +64,10 @@ public class FinalProgram {
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
         glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
-        // glOrtho(0, 640, 0, 480, 1, -1); // First Quadrant only
-        glOrtho(-320, 320, -240, 240, 1, -1); // Centered
+        GLU.gluPerspective(100.0f, (float)displayMode.getWidth()/(float)
+        displayMode.getHeight(), 0.1f, 300.0f);
         glMatrixMode(GL_MODELVIEW);
-        glHint(GL_PERSPECTIVE_CORRECTION_HINT,
-        GL_NICEST);
-    }
-    
-    // method: render
-    // purpose: Draw on the screen.
-    // W - Moves the camera up
-    // S - Moves the camera down
-    // A - Moves the camera left
-    // D - Moves the camera right
-    // Q - Rotates the camera counter-clockwise
-    // E - Rotates the camera clockwise
-    // Pressing Esc - Exits the window and program.
-    private void render() {
-        
-        while (!Display.isCloseRequested()) {
-            try{
-                    processInput();
-                    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-                    glLoadIdentity();
-                    // glPointSize(1); // Don't need point size
-                    
-                    glPushMatrix();
-                    
-                    // Move camera up, down, left and/or right
-                    glTranslatef(dx,dy,0f);
-                    
-                    // Roll
-                    glRotatef(0f+roll,0,0,1);
-                    
-                    // glColor3f(0f, 1f, 0f); // Green
-                    
-                    // This code is for 3D transformations (Lecture 7)
-//                    glTranslatef(2f, 2f, 4f); // Translate (2,2,4)
-//                    glTranslatef(2f, 2f, 2f); // Translate (2,2,2) Since cube is centered here.
-//                    glScalef(1f, 2f, 3f); // Scale (1,2,3)
-//                    glRotatef(90f, 0f, 0f, 1f); // Rotate 90 around z-axis
-//                    glRotatef(60f, 0f, 1f, 0f); // Rotate 60 around y-axis
-//                    glRotatef(30f, 1f, 0f, 0f); // Rotate 30 around x-axis
-//                    glTranslatef(-2f, -2f, -2f); // Translate (2,2,2) Since cube is centered here, need to translate back.
-                    glBegin(GL_QUADS);
-
-                    glColor3f(1.0f,1.0f,0.0f);
-                    glVertex3f( 100.0f,-100.0f,0f);
-                    glColor3f(1.0f,0.0f,0.0f);
-                    glVertex3f(-100.0f,-100.0f,0.0f);
-                    glColor3f(0.0f,0.0f,1.0f);
-                    glVertex3f(-100.0f, 100.0f,0.0f);
-                    glColor3f(0f,1f,0f);
-                    glVertex3f( 100.0f, 100.0f,0.0f);
-                    
-                    glEnd();
-                    glPopMatrix();
-                    Display.update();
-                    Display.sync(60);
-            }
-            catch(Exception e){
-            }
-        }
-        
-        Display.destroy();
-    }
-    
-    // 2D Viewing (Lecture 8)
-    // method: processInput
-    // purpose: Takes input if a key is pressed
-    // W - Moves the camera up
-    // S - Moves the camera down
-    // A - Moves the camera left
-    // D - Moves the camera right
-    // Q - Rotates the camera counter-clockwise
-    // E - Rotates the camera clockwise
-    // Esc - Exits the program
-    public void processInput(){
-        if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
-            dy++;
-        }
-        if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
-            dy--;
-        }
-        if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
-            dx--;
-        }
-        if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
-            dx++;
-        }
-        if (Keyboard.isKeyDown(Keyboard.KEY_E)) {
-            roll++;
-        }
-        if (Keyboard.isKeyDown(Keyboard.KEY_Q)) {
-            roll--;
-        }
-
-        // Esacpe key - Exits the window and program.            
-        if (Keyboard.isKeyDown(Keyboard.KEY_ESCAPE))
-        {
-            System.exit(0);
-        }
+        glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
     }
     
     // method: main
