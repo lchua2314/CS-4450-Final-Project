28c88517de69b4ea12d6f04357eb59c2b2f25e48
mkyuyang <46985141+mkyuyang@users.noreply.github.com>
2020-10-06 14:16:33 -0700

    Update Vector3Float.java

--- Vector3Float.java
+++ Vector3Float.java
@@ -1,5 +1,19 @@
+/***************************************************************
+* file: FinalProgram.java
+* author: L. Chua, A. Sun, M. Yang
+* class: CS 4450 – Computer Graphics
+*
+* assignment: final program
+* date last modified: 10/4/20
+*
+* purpose: This program creates a blockly game.
+*
+****************************************************************/
 package finalprogram;
 
+//method: Vector3Float
+//purpose: Constructor that hold user's camera's position
+//in 3D space.
 public class Vector3Float {
     
     public float x, y, z;
