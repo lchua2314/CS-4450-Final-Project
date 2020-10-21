601ff9b57d1bca7d8f7e324636d94196e2a89de9
mkyuyang <46985141+mkyuyang@users.noreply.github.com>
2020-10-20 22:30:12 -0700

    Update Block.java

--- Block.java
+++ Block.java
@@ -15,7 +15,7 @@ public class Block {
     private boolean IsActive;
     private BlockType Type;
     private float x,y,z;
-    
+
     public enum BlockType {
         BlockType_Grass(0),
         BlockType_Sand(1),
@@ -24,32 +24,32 @@ public class Block {
         BlockType_Stone(4),
         BlockType_Bedrock(5),
         BlockType_Default(6); // Added Default block since Chunk.java needed it. Line 151
-        
+
         private int BlockID;
-        
+
         BlockType(int i) {
         BlockID = i;
         }
-        
+
         //method: GetID
         //purpose: Getter for ID
         public int GetID(){
             return BlockID;
         }
-        
+
         //method: SetID
         //purpose: Setter for ID
         public void SetID(int i){
             BlockID = i;
         }
     }
-
+    
     //method: Block
     //purpose: Constructor for block
     public Block(BlockType type){
         Type= type;
     }
-    
+
     //method: setCoords
     //purpose: Sets the coorodinates for blocks
     public void setCoords(float x, float y, float z){
@@ -57,21 +57,21 @@ public class Block {
         this.y = y;
         this.z = z;
     }
-
+    
     //method: IsActive
-    //purpose: Getter for IsActive    
+    //purpose: Getter for IsActive
     public boolean IsActive() {
         return IsActive;
     }
-
+    
     //method: SetActive
-    //purpose: Setter for IsActive    
+    //purpose: Setter for IsActive
     public void SetActive(boolean active){
         IsActive = active;
     }
-
+    
     //method: GetID
-    //purpose: Getter for ID    
+    //purpose: Getter for ID
     public int GetID(){
         return Type.GetID();
     }
